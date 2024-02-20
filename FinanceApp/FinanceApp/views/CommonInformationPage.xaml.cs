94d95ce75fe6a63a135d56291b03aef6d646a41c
korolenko_danila <korolenko.danila1@gmail.com>
2024-02-20 15:10:43 +0300

    qqqq

---
+++ FinanceApp/FinanceApp/views/CommonInformationPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FinanceApp.views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class CommonInformationPage : ContentPage
+	{
+		public CommonInformationPage ()
+		{
+			InitializeComponent ();
+			card.Source = ImageSource.FromResource("FinanceApp.icons.card.png");
+            cathegory.Source = ImageSource.FromResource("FinanceApp.icons.cathegories.png");
+            list.Source = ImageSource.FromResource("FinanceApp.icons.list.png");
+			diagram.Source = ImageSource.FromResource("FinanceApp.icons.diagram.png");
+			change.Source = ImageSource.FromResource("FinanceApp.icons.change.png");
+			arrow_L.Source = ImageSource.FromResource("FinanceApp.icons.arrow_to_l.png");
+			arrow_R.Source = ImageSource.FromResource("FinanceApp.icons.arrow_to_r.png");
+		}
+
+        private async void ToCardPage(object sender, EventArgs e)
+        {
+			await Navigation.PushAsync(new MainPage()); 
+        }
+    }
+}
---
+++ FinanceApp/FinanceApp/views/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using FinanceApp.views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace FinanceApp
+{
+    public partial class MainPage : ContentPage/*, INotifyPropertyChanged*/
+    {
+        public string color = "#FFF7EC";
+        //public string Color
+        //{
+        //    get { return _color; }
+        //    set
+        //    {
+        //        if (_color != value)
+        //        {
+        //            _color = value;
+        //            OnPropertyChanged(nameof(Color));
+        //        }
+        //    }
+        //}
+
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+        }
+   
+
+    private void PrintSize(object sender, EventArgs e)
+        {
+            double width = Application.Current.MainPage.Width;
+            double height = Application.Current.MainPage.Height;
+            Console.WriteLine(width);
+            Console.WriteLine(height);
+            Console.WriteLine("!!!!!!!!!!!!!!!!!!!!");
+        }
+
+        private async void ToPageOfCommonInformation(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new CommonInformationPage());
+        }
+
+        //private async void ToPassPage(object sender, EventArgs e)
+        //{
+        //    await Navigation.PushAsync(new Pas());
+        //}
+    }
+}
