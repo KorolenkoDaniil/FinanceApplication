acc06b80a0a88f5e81e448861b6cf51b97a8417b
korolenko_danila <korolenko.danila1@gmail.com>
2024-02-21 15:18:31 +0300

    layout of converter done

--- FinanceApp/FinanceApp/views/ConvertPage.xaml.cs
+++ FinanceApp/FinanceApp/views/ConvertPage.xaml.cs
@@ -1,26 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
+
 namespace FinanceApp.views
 {
-    public partial class CalculatorPage : ContentPage
+    public partial class CovertPage : ContentPage
     {
-        public CalculatorPage()
+        public CovertPage()
         {
-            InitializeComponent(); 
+            InitializeComponent();
             card.Source = ImageSource.FromResource("FinanceApp.icons.card.png");
-            cathegory.Source = ImageSource.FromResource("FinanceApp.icons.cathegories.png"); 
+            cathegory.Source = ImageSource.FromResource("FinanceApp.icons.cathegories.png");
             list.Source = ImageSource.FromResource("FinanceApp.icons.list.png");
             diagram.Source = ImageSource.FromResource("FinanceApp.icons.diagram.png");
             change.Source = ImageSource.FromResource("FinanceApp.icons.change1.png");
             delete.Source = ImageSource.FromResource("FinanceApp.icons.delete-right.png");
-        }
-        private void change_Clicked(object sender, EventArgs e)
-        {
         }
 
         private async void ToListPage(object sender, EventArgs e)
@@ -28,14 +21,14 @@ namespace FinanceApp.views
             await Navigation.PushAsync(new CommonInformationPage());
         }
 
-        private void ToCalcuatorPage(object sender, EventArgs e)
+        private async void ToCalcuatorPage(object sender, EventArgs e)
         {
-            //сброс данных 
+            await Navigation.PushAsync(new CalculatorPage()); 
         }
 
-        private async void ToConvertPage(object sender, EventArgs e)
+        private void ToConvertPage(object sender, EventArgs e)
         {
-            await Navigation.PushAsync(new CovertPage());
+            //сброс данных 
         }
     }
 }
