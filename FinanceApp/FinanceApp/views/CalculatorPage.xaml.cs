16d8e35906dc96dcda0182122a7b2f8f751a9b59
korolenko_danila <korolenko.danila1@gmail.com>
2024-02-21 14:48:24 +0300

    layout with ConvertPage

--- FinanceApp/FinanceApp/views/CalculatorPage.xaml.cs
+++ FinanceApp/FinanceApp/views/CalculatorPage.xaml.cs
@@ -3,29 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-
 namespace FinanceApp.views
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class CalculatorPage : ContentPage
-	{
-		public CalculatorPage ()
-		{
-			InitializeComponent ();
+    public partial class CalculatorPage : ContentPage
+    {
+        public CalculatorPage()
+        {
+            InitializeComponent(); 
             card.Source = ImageSource.FromResource("FinanceApp.icons.card.png");
-            cathegory.Source = ImageSource.FromResource("FinanceApp.icons.cathegories.png");
+            cathegory.Source = ImageSource.FromResource("FinanceApp.icons.cathegories.png"); 
             list.Source = ImageSource.FromResource("FinanceApp.icons.list.png");
             diagram.Source = ImageSource.FromResource("FinanceApp.icons.diagram.png");
             change.Source = ImageSource.FromResource("FinanceApp.icons.change1.png");
             delete.Source = ImageSource.FromResource("FinanceApp.icons.delete-right.png");
         }
-
         private void change_Clicked(object sender, EventArgs e)
         {
+        }
 
+        private async void ToListPage(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new CommonInformationPage());
         }
     }
 }
